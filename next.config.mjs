b68da1611cf394b758424bb8a71a8d7320b357f9
Lucas Zapico <dev@lucaszapico.space>
2024-09-14 08:27:48 -0700

    feat(config) support fs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+/**
+ * 
+ */
+export const projectDir = process.cwd()
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // config.infrastructureLogging = { debug: /PackFileCache/ },
+    config.resolve.fallback = { ...config.resolve.fallback, fs: false };
+
+    return config;
+  }
+};
 
 export default nextConfig;
