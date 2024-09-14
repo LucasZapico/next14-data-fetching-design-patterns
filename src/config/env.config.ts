33f7e94c9c806abe319d694844ff55d5e38146f4
Lucas Zapico <dev@lucaszapico.space>
2024-09-14 08:27:34 -0700

    feat(config) update
    - return object

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,7 +1,13 @@
-const defaultEnv = () => {
-  NODE_ENV: process.env.NODE_ENV || "development",
-  PORT: process.env.PORT || 3344,
-};
+import { loadEnvConfig } from "@next/env";
+import { projectDir } from "../../next.config.mjs";
 
+loadEnvConfig(projectDir);
+
+const defaultEnv = () => {
+  return {
+    NODE_ENV: process.env.NODE_ENV || "development",
+    PORT: process.env.PORT || 3344,
+  };
+};
 
 export default defaultEnv;
