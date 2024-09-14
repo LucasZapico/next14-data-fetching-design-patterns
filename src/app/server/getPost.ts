488341ee255af71c62bf90cb501e3bd2d20013a7
Lucas Zapico <dev@lucaszapico.space>
2024-09-14 08:27:09 -0700

    feat(log) add simple check in action

--- src/app/server/getPost.ts
+++ src/app/server/getPost.ts
@@ -1,8 +1,9 @@
 "use server";
+import { logInfo } from "@/utils/cliLogger";
 
 export const getPosts = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await response.json();
-
+  logInfo("data from server action getPosts", { data });
   return data;
 };
